3dd9107952480d74ad6344b00e03a75b222ff117
simjaeryun <simjaeding@gmail.com>
2021-07-08 19:18:07 +0900

    Fifth task

---
+++ assignment/five/task5.js
@@ -0,0 +1,36 @@
+const body = document.querySelector("body");
+const btn = body.querySelector("button");
+const colors = [
+  "#ef5777",
+  "#575fcf",
+  "#4bcffa",
+  "#34e7e4",
+  "#0be881",
+  "#f53b57",
+  "#3c40c6",
+  "#0fbcf9",
+  "#00d8d6",
+  "#05c46b",
+  "#ffc048",
+  "#ffdd59",
+  "#ff5e57",
+  "#d2dae2",
+  "#485460",
+  "#ffa801",
+  "#ffd32a",
+  "#ff3f34",
+];
+
+function handleColorChange() {
+  const firstColor = colors[Math.floor(Math.random() * colors.length)];
+  const secondColor = colors[Math.floor(Math.random() * colors.length)];
+  const ifSameColor = colors[Math.ceil(Math.random() * colors.length)];
+
+  if (firstColor === secondColor) {
+    body.style.background = `linear-gradient(to right, ${firstColor}, ${ifSameColor})`;
+  } else {
+    body.style.background = `linear-gradient(to right, ${firstColor}, ${secondColor})`;
+  }
+}
+
+btn.addEventListener("click", handleColorChange);
